044199f93b76be4d62c47e7215848fe6e32e355e
Mdelvalle <mdelvalle@live.com>
2015-02-24 17:43:12 -0600

    Slider is up but not yet working

--- script.js
+++ script.js
@@ -4,41 +4,64 @@
  *
  */
 
-function load() {
-  $('#slider-range').slider({
-    range: true,
-    min: 0,
-    max: 1,
-    values: [1000, 5000],
-    slide: function(event, ui) {
-      $('#startNumber').val
-    }
-  })
+/*
+ * FIND OUT HOW TO INITIALIZE EVERYTHING
+ * AFTER THE WIDGET IS READY
+ *
+ */
 
+function load() {
   var widget = SC.Widget(document.getElementById('soundcloud_widget'));
-  var startPos = document.getElementById('slider-range').slider(); 
-  var endPos = document.getElementById('endNumber').value;
+  var songDuration = 0;
+  console.log('songduration0', songDuration);
 
   // The Soundcloud widget has loaded.
-  widget.bind(SC.Widget.Events.READY, function() {
-      console.log('Ready...');
+  var ready = new Promise(widget.bind(SC.Widget.Events.READY, function() {
+    console.log('Ready...');
+    console.log('songduration1', songDuration);
   });
+
+  console.log('songduration3', songDuration);
+    //songDuration = duration;
+    console.log('songduration', songDuration);
+
+  // Initialize nstSlider class attributes
+  ready
+  $('.nstSlider')[0].setAttribute('data-range_min', 38000);
+  $('.nstSlider')[0].setAttribute('data-range_max', 55000);
+  $('.nstSlider')[0].setAttribute('data-cur_min', 38000);
+  $('.nstSlider')[0].setAttribute('data-cur_max', 55000);
+
+  $('.nstSlider').nstSlider({
+    'crossable_handles': false,
+    'left_grip_selector': '.leftGrip',
+    'right_grip_selector': '.rightGrip',
+    'value_bar_selector': '.bar',
+    'value_changed_callback': function(cause, leftValue, rightValue) {
+      $('.leftLabel').text(leftValue);
+      $('.rightLabel').text(rightValue);
+    }
+  });
+
+  var startPos = $('.nstSlider').nstSlider('get_current_min_value');
+  console.log(startPos);
+
+  var endPos = $('.nstSlider').nstSlider('get_current_max_value');
+  console.log(endPos);
 
   // When the Soundcloud widget has started to play
   // seek to the START position
   widget.bind(SC.Widget.Events.PLAY, function() {
-      widget.seekTo(startPos);
+    widget.seekTo(startPos);
   });
 
   // Return to START position when the song reaches
   // the STOP position
   widget.bind(SC.Widget.Events.PLAY_PROGRESS, function(e) {
-      if (e.currentPosition >= endPos) {
-          widget.seekTo(startPos);
-      }
+    if (e.currentPosition >= endPos) {
+      widget.seekTo(startPos);
+    }
   });
-
-
 }
 
 window.onload = load;
