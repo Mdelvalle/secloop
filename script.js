2855a377fcda70fef0f1a6803b42ab90db6b2303
Mdelvalle <mdelvalle@live.com>
2015-03-12 16:50:55 -0500

    Slider works and is in tune with the SC widget

--- script.js
+++ script.js
@@ -13,41 +13,49 @@
 function load() {
   var widget = SC.Widget(document.getElementById('soundcloud_widget'));
   var songDuration = 0;
-  console.log('songduration0', songDuration);
 
   // The Soundcloud widget has loaded.
-  var ready = new Promise(widget.bind(SC.Widget.Events.READY, function() {
-    console.log('Ready...');
-    console.log('songduration1', songDuration);
+  var widgetReady = new Promise(function(resolve, reject) {
+
+    widget.bind(SC.Widget.Events.READY, function() {
+      resolve();
+    });
   });
 
-  console.log('songduration3', songDuration);
-    //songDuration = duration;
-    console.log('songduration', songDuration);
-
+  var startPos = 0;
+  var endPos = 0;
   // Initialize nstSlider class attributes
-  ready
-  $('.nstSlider')[0].setAttribute('data-range_min', 38000);
-  $('.nstSlider')[0].setAttribute('data-range_max', 55000);
-  $('.nstSlider')[0].setAttribute('data-cur_min', 38000);
-  $('.nstSlider')[0].setAttribute('data-cur_max', 55000);
+  widgetReady.then(function () {
+    var durationReady = new Promise(function(resolve, reject) {
+      widget.getDuration(function(dur) {
+        songDuration = dur;
+        resolve();
+      });
+    });
 
-  $('.nstSlider').nstSlider({
-    'crossable_handles': false,
-    'left_grip_selector': '.leftGrip',
-    'right_grip_selector': '.rightGrip',
-    'value_bar_selector': '.bar',
-    'value_changed_callback': function(cause, leftValue, rightValue) {
-      $('.leftLabel').text(leftValue);
-      $('.rightLabel').text(rightValue);
-    }
+    durationReady.then(function() {
+      $('.nstSlider')[0].setAttribute('data-range_min', 0);
+      $('.nstSlider')[0].setAttribute('data-range_max', songDuration);
+      $('.nstSlider')[0].setAttribute('data-cur_min', 0);
+      $('.nstSlider')[0].setAttribute('data-cur_max', songDuration);
+
+      $('.nstSlider').nstSlider({
+        'crossable_handles': false,
+        'left_grip_selector': '.leftGrip',
+        'right_grip_selector': '.rightGrip',
+        'value_bar_selector': '.bar',
+        'value_changed_callback': function(cause, leftValue, rightValue) {
+          widget.seekTo(leftValue);
+          startPos = leftValue;
+          endPos = rightValue;
+
+          $('.leftLabel').text(leftValue);
+          $('.rightLabel').text(rightValue);
+        }
+      });
+    });
   });
 
-  var startPos = $('.nstSlider').nstSlider('get_current_min_value');
-  console.log(startPos);
-
-  var endPos = $('.nstSlider').nstSlider('get_current_max_value');
-  console.log(endPos);
 
   // When the Soundcloud widget has started to play
   // seek to the START position
@@ -58,8 +66,10 @@ function load() {
   // Return to START position when the song reaches
   // the STOP position
   widget.bind(SC.Widget.Events.PLAY_PROGRESS, function(e) {
-    if (e.currentPosition >= endPos) {
+    var currPos = e.currentPosition;
+    if (currPos >= endPos || currPos === songDuration) {
       widget.seekTo(startPos);
+      widget.play();
     }
   });
 }
